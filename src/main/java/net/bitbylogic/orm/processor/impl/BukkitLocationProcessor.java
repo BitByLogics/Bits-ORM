04c20e40c1c62bd93e48d5d1cdd16f12574b867c
Justin <33465177+BitByLogics@users.noreply.github.com>
2025-01-03 05:30:30 -0500

    feat: Implement BukkitLocationProcessor

---
+++ src/main/java/net/bitbylogic/orm/processor/impl/BukkitLocationProcessor.java
@@ -0,0 +1,24 @@
+package net.bitbylogic.orm.processor.impl;
+
+import net.bitbylogic.orm.processor.HikariFieldProcessor;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+
+public class BukkitLocationProcessor implements HikariFieldProcessor<Location> {
+
+    @Override
+    public Object parseToObject(Location location) {
+        if(location.getWorld() == null) {
+            return "NULL";
+        }
+
+        return String.format("%s:%s:%s:%s", location.getWorld().getName(), location.getBlockX(), location.getBlockY(), location.getBlockZ());
+    }
+
+    @Override
+    public Location parseFromObject(Object object) {
+        String[] splitArgs = ((String) object).split(":");
+        return new Location(Bukkit.getWorld(splitArgs[0]), Double.parseDouble(splitArgs[1]), Double.parseDouble(splitArgs[2]), Double.parseDouble(splitArgs[3]));
+    }
+
+}
